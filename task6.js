84b6940a86ed7acc2ace5944b6b52f4dcd0b69c3
bipin7745 <tanytask1919@gmail.com>
2025-09-08 15:53:44 +0530

    Add all file JavaScipt day-8

---
+++ task6.js
@@ -0,0 +1,34 @@
+// JavaScript Number Examples
+
+let intNum = 42;
+
+
+let floatNum = 3.14159;
+    
+let expNum = 1.5e4; 
+
+let binaryNum = 0b1010; 
+
+
+let octalNum = 0o755; 
+
+
+let hexNum = 0xFF; 
+
+
+let bigIntNum = 1234567890123456789012345678901234567890n;
+
+let inf = Infinity;
+let negInf = -Infinity;
+let notANumber = NaN;
+
+console.log("Integer:", intNum);
+console.log("Float:", floatNum);
+console.log("Exponential:", expNum);
+console.log("Binary:", binaryNum);
+console.log("Octal:", octalNum);
+console.log("Hexadecimal:", hexNum);
+console.log("BigInt:", bigIntNum);
+console.log("Infinity:", inf);
+console.log("-Infinity:", negInf);
+console.log("NaN:", notANumber);
